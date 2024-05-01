233c6000d309052465e329862d36f9f3111c6f09
mai033 <145622463+mai033@users.noreply.github.com>
2024-05-01 16:34:38 -0700

    sixth commit

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -1,17 +1,9 @@
-import React, { ReactNode } from 'react';
-
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
-
-import Image from 'next/image';
+'use client';
+import { ReactNode } from 'react';
+import { Dialog, DialogContent } from './ui/dialog';
 import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
+import { Button } from './ui/button';
+import Image from 'next/image';
 
 interface MeetingModalProps {
   isOpen: boolean;
@@ -19,10 +11,12 @@ interface MeetingModalProps {
   title: string;
   className?: string;
   children?: ReactNode;
-  image?: string;
-  buttonIcon?: string;
-  buttonText?: string;
   handleClick?: () => void;
+  buttonText?: string;
+  instantMeeting?: boolean;
+  image?: string;
+  buttonClassName?: string;
+  buttonIcon?: string;
 }
 
 const MeetingModal = ({
@@ -31,19 +25,20 @@ const MeetingModal = ({
   title,
   className,
   children,
-  image,
-  buttonIcon,
-  buttonText,
   handleClick,
+  buttonText,
+  instantMeeting,
+  image,
+  buttonClassName,
+  buttonIcon,
 }: MeetingModalProps) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogTrigger>Open</DialogTrigger>
       <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
         <div className="flex flex-col gap-6">
           {image && (
             <div className="flex justify-center">
-              <Image src={image} alt="image" width={72} height={72} />
+              <Image src={image} alt="checked" width={72} height={72} />
             </div>
           )}
           <h1 className={cn('text-3xl font-bold leading-[42px]', className)}>
@@ -51,7 +46,9 @@ const MeetingModal = ({
           </h1>
           {children}
           <Button
-            className="bg-blue-1 focus-visible:ring-0 focus-visible: ring-offset-0"
+            className={
+              'bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0'
+            }
             onClick={handleClick}
           >
             {buttonIcon && (
@@ -61,7 +58,8 @@ const MeetingModal = ({
                 width={13}
                 height={13}
               />
-            )} &nbsp;
+            )}{' '}
+            &nbsp;
             {buttonText || 'Schedule Meeting'}
           </Button>
         </div>
